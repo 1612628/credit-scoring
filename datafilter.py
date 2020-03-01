edf6573b6d87e468cb33ae1b7bfc71ab53e4d865
thanhnd0101 <thanhnguyenduy2304@gmail.com>
2020-03-01 21:16:31 +0700

    change classes name: Encoder - cs_encoder, DataGenerate - cs_data_generate, DataFilter - cs_data_filter

--- datafilter.py
+++ datafilter.py
@@ -5,7 +5,7 @@ import pandas as pd
 import gc
 
 # DataFilter class for filter opration on data
-class cs_datafilter():
+class cs_data_filter():
     """ DataFilter
     
     Used for filter opration on data
